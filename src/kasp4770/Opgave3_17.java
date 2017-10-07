81256bd6168ed2af549232dae1b1ae7d1c7e73a8
kasp4770 <kasp4770@edu.easj.dk>
2017-10-07 18:44:45 +0200

    Opgave 3.17

--- src/kasp4770/Opgave3_17.java
+++ src/kasp4770/Opgave3_17.java
@@ -2,6 +2,15 @@ package kasp4770;
 
 import java.util.Scanner;
 
+/*
+(Game: scissor, rock, paper) Write a program that plays the popular scissor-rockpaper
+game. (A scissor can cut a paper, a rock can knock a scissor, and a paper can
+wrap a rock.) The program randomly generates a number 0, 1, or 2 representing
+scissor, rock, and paper. The program prompts the user to enter a number 0, 1, or
+2 and displays a message indicating whether the user or the computer wins, loses,
+or draws.
+A Danish version.
+*/
 public class Opgave3_17 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in); //scanner for userinput in console
@@ -10,11 +19,12 @@ public class Opgave3_17 {
 
         //System.out.println("Computerens valg: " + randomNumber); //Cheatcode or for testing randomNumber
 
-        //
+        //Prompt the user to enter value between 0-2
         System.out.println("Indast tal for Sten, saks, eller papir");
         System.out.println("Sten(0), saks(1), papir(2)");
         int yourNumber = input.nextInt();
 
+        //Displaying result
         if (yourNumber == 0){
             System.out.print("Du valgte sten! ");
             if (randomNumber == 0){
@@ -61,6 +71,7 @@ public class Opgave3_17 {
             }
 
         }
+        //Active if the user enters a number <0 or >2
         else if (yourNumber < 0 || yourNumber > 2) {
             System.out.println("Du har ikke valgt nummer 0, 1 eller 2. Prøv igen!");
         }
