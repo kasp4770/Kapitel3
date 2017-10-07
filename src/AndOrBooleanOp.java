edbe3e93241a5f7590404f6c7c900acb6fe596cd
kasp4770 <kasp4770@edu.easj.dk>
2017-10-07 15:50:48 +0200

    En lille hjælp til forståelse af Boolean operatørerne

--- src/AndOrBooleanOp.java
+++ src/AndOrBooleanOp.java
@@ -1,23 +1,23 @@
 public class AndOrBooleanOp {
-    /*
+/*
     Et program til forståelse af forskellen mellem 'and (&&)' og 'or(||)' Boolean operatørerne, eftersom jeg selv skulle
     skrive det ned for at forstå forskellen 100%. Nu tænker jeg selvfølgelig at det er logik for burhøns! xD
 
     Også et eksempel fra 3.28 (side 119) som viser 'endten eller(^)' operatoren.
 
-                P1:        P2:
-    && eks.:    x < y  &&  y < z     - Hvis P1 er 'true' evaluerer Java P2. Ikke hvis P1 er
-                                           'false'. Så hvis P1 er 'false' er hele statement 'false'.
-
-                P1:        P2:
-    || eks.:    x < y  ||  y < z     - Java evaluerer P1, og hvis P1 er 'false' evaluerer Java P2. Hvis P1 er
-                                           'true' evaluerer Java ikke P2. Så hvis en af dem er 'true' er hele statement
-                                           'true'
-
-    ^ eks.:     P1:        P2:
-                x < y  ^   y < z     - Hvis P1 er 'true' og P2 er 'true', er statement 'false'. En af dem skal
-                                           afvige fra den anden, 'true/false' eller 'false/true'
-    */
+            P1:        P2:
+    &&:     x < y  &&  y < z     - Hvis P1 er 'true' evaluerer Java P2. Ikke hvis P1 er
+                                   'false'. Så hvis P1 er 'false' er hele statement 'false'.
+------------------------------------------------------------------------------------------------------------------------
+            P1:        P2:
+    ||:     x < y  ||  y < z     - Java evaluerer P1, og hvis P1 er 'false' evaluerer Java P2. Hvis P1 er
+                                   'true' evaluerer Java ikke P2. Så hvis en af dem er 'true' er hele statement
+                                   'true'
+------------------------------------------------------------------------------------------------------------------------
+            P1:        P2:
+    ^ :     x < y  ^   y < z     - Hvis P1 er 'true' og P2 er 'true', er statement 'false'. En af dem skal
+                                   afvige fra den anden, 'true/false' eller 'false/true'
+*/
 
 
     public static void main(String[] args) {
@@ -31,9 +31,12 @@ public class AndOrBooleanOp {
     2, 3 og 2.
     */
         System.out.println("Indsæt x, y og z: (3.24 side 118)");
-        double x = input.nextDouble();
-        double y = input.nextDouble();
-        double z = input.nextDouble();
+        System.out.print("x: ");
+        int x = input.nextInt();
+        System.out.print("y: ");
+        int y = input.nextInt();
+        System.out.print("z: ");
+        int z = input.nextInt();
 
         System.out.println("(x < y && y < z) is " + (x < y && y < z));
         System.out.println("(x < y || y < z) is " + (x < y || y < z));
