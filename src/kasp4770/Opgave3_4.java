eea3a56e01fd1882840c9ae2f725258715c0d4bf
kasp4770 <kasp4770@edu.easj.dk>
2017-10-07 16:54:54 +0200

    opgave 3.4 - med dokumentation

--- src/kasp4770/Opgave3_4.java
+++ src/kasp4770/Opgave3_4.java
@@ -1,11 +1,19 @@
 package kasp4770;
 
+/*
+Exercise 3.4 - (Random month) Write a program that randomly generates an integer between 1 and 12 and displays the
+English month name January, February, …, December for the number 1, 2, …, 12, accordingly.
+*/
 public class Opgave3_4 {
     public static void main(String[] args) {
 
+        //Random number between 0-12 +1. (Math.random = random number between 0.0 - 0.9. Multiplied by 12 it is between
+        //0.0 - 10.8. By making it an int value, we get a whole number between 0 - 11, and by adding 1 we avoid 0 and
+        //11.8 is rounded up to 12. I think... András am I right or nah?
         int number = ((int) (Math.random() * 12))+1;
         System.out.print("måned nr. " + number + " er ");
 
+        //Switch statement to print out text for the given value
         switch (number) {
             case 1:
                 System.out.print("januar");break;
